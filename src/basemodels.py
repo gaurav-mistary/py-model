44f737cd6916c2b0711035e5c39612f163dbd40b
Gaurav Mistary <gm179gaurav@gmail.com>
2023-08-31 17:44:26 +0530

    MAIN 1.1 correct ImmutableModel.new

--- src/basemodels.py
+++ src/basemodels.py
@@ -18,7 +18,7 @@ class MutableModel(Model):
 class ImmutableModel(MutableModel):
     model_config = ConfigDict(extra="forbid", frozen=True)
 
-    def _copy(self, key: str, value: T) -> Self:
+    def _copy(self, key: str, value: T, deep: bool = False) -> Self:
         """
         The core of my recursive genius!
 
@@ -66,40 +66,43 @@ class ImmutableModel(MutableModel):
             model_attr_name = key_split[-2]  # this will raise the IndexError
             attr_name = key_split[-1]
 
-            if (_obj := self.model_fields.get(model_attr_name)) is None:
+            if (_obj := getattr(self, model_attr_name)) is None:
                 raise AttributeError(
                     f"[X] Field: {key} is missing attribute: {model_attr_name}"
                 )
 
-            if not hasattr(_obj.default, "model_copy"):
+            if not hasattr(_obj, "model_copy"):
+                print(f"OBJ: {_obj}")
                 raise ValueError(
                     f"[X] Key: {key} includes a non-pydantic model class "
-                    f"attribute: {model_attr_name}, with class: {_obj.default.__class__}"
+                    f"attribute: {model_attr_name}, with class: {_obj.__class__}"
                 )
 
             return self._copy(
                 key=".".join(key_split[:-1]),
-                value=_obj.default.model_copy(update={attr_name: value}),
+                value=_obj.model_copy(update={attr_name: value}, deep=deep),
+                deep=deep,
             )
 
         except IndexError:
             model_attr_name = key_split.pop(0)
             _obj = self.model_fields.get(model_attr_name)
-            return self.model_copy(update={model_attr_name: value})
+            return self.model_copy(update={model_attr_name: value}, deep=deep)
 
-    def new(self, key: str, value: T) -> Self:
+    def new(self, key: str, value: T, deep: bool = False) -> Self:
         """
         This is probably my best contribution to recursive coding,
-        The new method series on internal _copy method which in-turn relies on Model.model_copy method
+        The new method relies on internal _copy method which in-turn relies on Model.model_copy method
         so the reliance priority is:
             new -> _copy -> Model.model_copy
 
         This acts as a bridge between the _copy method for additional experiments!
-        :param key:
-        :param value:
-        :return:
+        :param deep: boolean for deep copy propagation of other attributes too.
+        :param key: name of the attribute to be changed during copy generation
+        :param value: value of the attribute to be set
+        :return: a new model of Self
         """
-        return self._copy(key=key, value=value)
+        return self._copy(key=key, value=value, deep=deep)
 
 
 TImm = TypeVar("TImm", bound=ImmutableModel)
